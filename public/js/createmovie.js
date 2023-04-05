7cb8c2da514492dfc8668d1441c807d51f64d82f
Eric <ericnguyen23@gmail.com>
2023-04-05 14:11:49 -0700

    added vars to pass the submission

--- public/js/createmovie.js
+++ public/js/createmovie.js
@@ -7,12 +7,19 @@ const newMovieHandler = async (event) => {
     .value.trim();
   const image_name = document.querySelector("#film-image").value.trim();
 
+  // hard-coding user id for now to make sure there are no errors
+  // we'll need to figure out how user_id gets passed in later
   const user_id = 1;
 
-  if (movie_name && movie_description && image_name) {
+  if (movie_name && movie_description && image_name && user_id) {
     const response = await fetch(`/api/movies`, {
       method: "POST",
-      body: JSON.stringify({ movie_name, movie_description, image_name }),
+      body: JSON.stringify({
+        movie_name,
+        movie_description,
+        image_name,
+        user_id,
+      }),
       headers: {
         "Content-Type": "application/json",
       },
