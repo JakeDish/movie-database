887ec0fabcc383a8d98fd719640657164062e7d4
Trizzie Huynh <putmind2822@gmail.com>
2023-04-05 14:47:00 -0700

    added the api for signUp

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const moviesRoutes = require("./moviesRoutes");
 const loginRoutes = require("./loginRoutes");
+const signUpRoutes = require("./signUpRoutes");
+
 // const userRoutes = require("");
 // const likesRoutes = require("");
 
 router.use("/movies", moviesRoutes);
 router.use("/login", loginRoutes);
+router.use("/signup", signUpRoutes);
 // router.use("/user", userRoutes);
 // router.use("/likes", likesRoutes);
 
