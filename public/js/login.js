e580a1831b5042448c62a6cd8103d4f1e08bf25c
Trizzie Huynh <putmind2822@gmail.com>
2023-04-05 21:18:12 -0700

    .

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,25 @@
-const loginFormHandler = async function(event) {
+const loginFormHandler = async function (event) {
   event.preventDefault();
 
+  const userEmail = document.querySelector("#email-login");
+  const passwordEl = document.querySelector("#password-login");
 
-  const userEmail = document.querySelector('#email-login');
-  const passwordEl = document.querySelector('#password-login');
-
-  const response = await fetch('/api/user/login', {
-    method: 'POST',
-    body: JSON.stringify({ 
-      email: userEmail.value, 
-      password: passwordEl.value, }),
-    headers: { 'Content-Type': 'application/json' },
+  const response = await fetch("/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+      email: userEmail.value,
+      password: passwordEl.value,
+    }),
+    headers: { "Content-Type": "application/json" },
   });
 
   if (response.ok) {
-
-    document.location.replace('/dashboard');
+    document.location.replace("/dashboard");
   } else {
-    alert('Failed to login');
+    alert("Failed to login");
   }
 };
 
 document
-  .querySelector('#login-form')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector("#login-form")
+  .addEventListener("submit", loginFormHandler);
