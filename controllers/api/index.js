0ca25141c9221b7725d249801942f7320e73c7d8
Trizzie Huynh <putmind2822@gmail.com>
2023-04-05 19:07:03 -0700

    added logout js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,16 @@
 const router = require("express").Router();
 const moviesRoutes = require("./moviesRoutes");
 const loginRoutes = require("./loginRoutes");
+const logoutRoutes = require("./logoutRoutes");
 const signUpRoutes = require("./signUpRoutes");
 
 // const userRoutes = require("");
 // const likesRoutes = require("");
 
+
 router.use("/movies", moviesRoutes);
 router.use("/login", loginRoutes);
+router.use("/logout", logoutRoutes);
 router.use("/signup", signUpRoutes);
 // router.use("/user", userRoutes);
 // router.use("/likes", likesRoutes);
