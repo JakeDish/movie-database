4180d4c0f1c43b082ba7bbccedc5a515280c9546
Jake Disharoon <jakedisharoon@gmail.com>
2023-04-07 13:19:21 -0700

    Likes functionality

--- controllers/api/likesRoutes.js
+++ controllers/api/likesRoutes.js
@@ -22,4 +22,17 @@ router.put("/:id", async (req, res) => {
   res.status(200).json(likedata)
 });
 
+router.post("/", async (req, res) => {
+  try{
+    const newLike = await Likes.create({
+      ... req.body, 
+      user_id: req.session.user_id,
+      });
+
+    res.status(200).json(newLike);
+    } catch (err) {
+    res.status(400).json(err);
+  }
+  });
+
 module.exports = router;
